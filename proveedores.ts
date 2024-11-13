bfa87a6fcfc7c15543d618ec5a2fdc6361a5b8dc
Aaron Martinez <aaronmartineez@gmail.com>
2024-11-13 20:46:51 -0300

    declaro constructor y getters  de la clase proveedor

--- proveedores.ts
+++ proveedores.ts
@@ -1,3 +1,27 @@
 export class Proveedor {
+ private nombre: string;
+ private telefono: number;
+ private id:number;    
+
+ constructor(nombre:string, telefono:number, id:number){
+    this.nombre = nombre;
+    this.telefono = telefono;
+    this.id = id;
+
+ }
+
+ //getters
+
+ getNombre():string{
+    return this.nombre;
+ }
+
+ getTelefono():number{
+    return this.telefono;
+ }
+
+ getId():number{
+    return this.id;
+ }
 
 }
